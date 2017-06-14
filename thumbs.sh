7ec27f60a5ceff83af06de5b4504e081e68d398c
jag10 <jota259@gmail.com>
2017-06-14 17:22:06 +0200

    fix thumbs.sh

--- thumbs.sh
+++ thumbs.sh
@@ -1,10 +1,12 @@
 article_images_dir="content/articles/img"
 thumbnails_dir="content/articles/img/thumbs/"
-thumbnail_size="72"
+thumbnail_size="140"
+force=true
 
 for i in $(find $article_images_dir -maxdepth 1 -type f \( -name "*.jpg" -or -name "*.png" \) ); do
-  if [ "$i" -nt "$thumbnails_dir$i" ] || [ ! -f "content/articles/img/thumbs/$i" ]; then
+  filename=$(basename $i)
+  if [ "$i" -nt "$thumbnails_dir$filename" ] || [ ! -f "$thumbnails_dir$filename" ] || [ "$force" = true ]; then
     echo "converting $i";
-    convert "$i" -thumbnail $thumbnail_size "$thumbnails_dir$(basename $i)";
+    convert "$i" -thumbnail $thumbnail_size -quality 100 "$thumbnails_dir$(basename $i)";
   fi
 done;
