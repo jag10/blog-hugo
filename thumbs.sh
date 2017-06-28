a5298b4e5fde600682952dc4908b9009eb5355a2
jag10 <jota259@gmail.com>
2017-06-28 17:33:54 +0200

    add low quality conversion for article images

--- thumbs.sh
+++ thumbs.sh
@@ -11,3 +11,17 @@ for i in $(find $article_images_dir -maxdepth 1 -type f \( -name "*.jpg" -or -na
     convert "$i" -thumbnail $thumbnail_size -quality 100 "$thumbnails_dir$(basename $i)";
   fi
 done;
+
+echo "generating low quality images"
+for i in $(find $article_images_dir -maxdepth 2 -mindepth 2 -type f \( -name "*.jpg" -or -name "*.png" \) ); do
+  filename=$(basename $i)
+  dirname=$(dirname $i)
+  if [ $dirname"/" = $thumbnails_dir ]; then
+    continue
+  fi
+  thumbs_dir=$dirname"/min/"
+  if [ "$i" -nt "$thumbs_dir$filename" ] || [ ! -f "$thumbs_dir$filename" ] || [ "$force" = true ]; then
+    echo "converting $i";
+    convert "$i" -quality 20 "$thumbs_dir$filename";
+  fi
+done;
